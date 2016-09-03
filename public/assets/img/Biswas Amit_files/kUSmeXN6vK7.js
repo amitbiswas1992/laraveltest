bde0c2f1778c73b2206363f599af7c214a7a41d2
Amit K Biswas <biswas1992@hotmail.com>
2016-09-02 20:02:50 -0400

    modified Public folder

---
+++ public/assets/img/Biswas Amit_files/kUSmeXN6vK7.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["ppMnX"]); }
+
+__d('NotificationsTabsModel',['EnterpriseLabsLoggingEvents','EnterpriseLabsTypedLogger'],(function a(b,c,d,e,f,g){if(c.__markCompiled)c.__markCompiled();var h={TABS:['All','Conversations','Keywords'],_onTabSelectionChanged:function i(){var j=h.TABS[this.getSelectedIndex()];new (c('EnterpriseLabsTypedLogger'))().setEntityID('tab:'+j).setEvent(c('EnterpriseLabsLoggingEvents').NT_CLICK_TAB_CONVERSATIONS).log();}};f.exports=h;}),null);
